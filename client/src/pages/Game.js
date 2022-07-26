1d86dba588b37722e7b5ade59b0a86dd139ef4fe
Mattadin <m4tt4d1n@gmail.com>
2022-07-26 13:04:58 +1000

    Movement allowed, math needs fixing

--- client/src/pages/Game.js
+++ client/src/pages/Game.js
@@ -1,3 +1,4 @@
+/* eslint-disable*/
 import React, { useState, useRef, useEffect } from 'react';
 import TUNDRA from '../img/map.png';
 import PALACE from '../img/map2.png';
@@ -15,17 +16,6 @@ import { Inventory, Item } from '../Inventory';
 import { io } from 'https://cdn.socket.io/4.4.1/socket.io.esm.min.js';
 
 const Game = () => {
-  const Img = {};
-  Img.player = new Image();
-  Img.player.src = PENGUIN;
-  Img.projectile = new Image();
-  Img.projectile.src = SNOWBALL;
-  Img.map = {};
-  Img.map['tundra'] = new Image();
-  Img.map['tundra'].src = TUNDRA;
-  Img.map['palace'] = new Image();
-  Img.map['palace'].src = PALACE;
-
   const WIDTH = 500;
   const HEIGHT = 500;
   const socket = io();
@@ -41,186 +31,263 @@ const Game = () => {
   const uiRef = useRef(null);
   // ctxUi.font = '30px Arial';
 
-  const [image, setImage] = useState(null);
+  // const [image, setImage] = useState(null);
+
+  // useEffect(() => {
+  //   const img = new Image();
+  //   img.src = TUNDRA;
+
+  //   img.onload = () => setImage(img);
+  // }, []);
 
   useEffect(() => {
-    const img = new Image();
-    img.src = TUNDRA;
+    // console.log('Rendering image');
+    const ctx = canvasRef.current.getContext('2d');
+    if (canvasRef.current) {
+      const Img = {};
+      Img.player = new Image();
+      Img.player.src = PENGUIN;
+      Img.projectile = new Image();
+      Img.projectile.src = SNOWBALL;
+      Img.map = {};
+      Img.map['tundra'] = new Image();
+      Img.map['tundra'].src = TUNDRA;
+      Img.map['palace'] = new Image();
+      Img.map['palace'].src = PALACE;
 
-    img.onload = () => setImage(img);
-  }, []);
+      // socket.on('addToChat', (data) => {
+      //   chatText.innerHTML += '<div>' + data + '</div>';
+      // });
 
-  useEffect(() => {
-    console.log({ image, canvasRef });
-    if (image && canvasRef) {
-      const ctx = canvasRef.current.getContext('2d');
-      ctx.fillStyle = 'black';
-      ctx.fillRect(0, 0, 500, 500);
-      ctx.drawImage(image, 0, 0, 500, 500);
-    }
-  }, [image, canvasRef]);
+      // socket.on('evalAnswer', (data) => {
+      //   console.log(data);
+      // });
 
-  socket.on('addToChat', (data) => {
-    chatText.innerHTML += '<div>' + data + '</div>';
-  });
+      // chatForm.onsubmit = (e) => {
+      //   e.preventDefault();
+      //   if (chatInput.value[0] === '/') {
+      //     socket.emit('evalServer', chatInput.values.slice(1));
+      //   } else {
+      //     socket.emit('sendMsgToServer', chatInput.value);
+      //   }
+      //   chatInput.value = '';
+      // };
 
-  // socket.on('evalAnswer', (data) => {
-  //   console.log(data);
-  // });
+      // UI Functions
 
-  // chatForm.onsubmit = (e) => {
-  //   e.preventDefault();
-  //   if (chatInput.value[0] === '/') {
-  //     socket.emit('evalServer', chatInput.values.slice(1));
-  //   } else if (chatInput.value[0] === '@') {
-  //     // Chat syntax: @username, message
-  //     socket.emit('sendPmToServer', {
-  //       userName: chatInput.value.slice(1, chatInput.value.indexOf(',')),
-  //       message: chatInput.value.slice(chatInput.value.indexOf(',') + 1),
-  //     });
-  //   } else {
-  //     socket.emit('sendMsgToServer', chatInput.value);
-  //   }
-  //   chatInput.value = '';
-  // };
+      // const changeMap = () => {
+      //   socket.emit('changeMap');
+      // };
 
-  // UI Functions
+      // game stuff
 
-  const changeMap = () => {
-    socket.emit('changeMap');
-  };
+      // init
 
-  let inventory = new Inventory(socket, false);
-  socket.on('updateInventory', (items) => {
-    inventory.items = items;
-    inventory.refreshRender();
-  });
+      // initializes a player using a package sent from server, contains all the data for client side
 
-  // game stuff
+      class Player {
+        constructor(initPack) {
+          let self = {};
+          self.id = initPack.id;
+          self.number = initPack.number;
+          self.x = initPack.x;
+          self.y = initPack.y;
+          self.hp = initPack.hp;
+          self.hpMax = initPack.hpMax;
+          self.level = initPack.level;
+          self.map = initPack.map;
+          // draws our characters, their hp bars and their levels.
+          self.draw = () => {
+            if (Player.list[selfId].map !== self.map) {
+              return;
+            }
+            let x = self.x - Player.list[selfId].x + WIDTH / 2;
+            let y = self.y - Player.list[selfId].y + HEIGHT / 2;
 
-  // init
+            let width = Img.player.width / 16;
+            let height = Img.player.height / 16;
 
-  // initializes a player using a package sent from server, contains all the data for client side
+            let hpWidth = (30 * self.hp) / self.hpMax;
+            ctx.fillStyle = 'red';
+            ctx.fillRect(x - hpWidth / 2, y - 40, hpWidth, 4);
+            // setting parameters for how to use the player images to render our user's sprites
 
-  Player.list = {};
-
-  // initializes a Projectile using a package sent from server, contains all the data for client side
-
-  Projectile.list = {};
-
-  // creates a new player and projectile and adds them to the player and projectile list
-  // expect: {player : [{id:123,number:'1',x:0,y:0},{id:1,number:'2',x:0,y:0}], projectile:[]}
-  let selfId = null;
-
-  socket.on('init', (data) => {
-    if (data.selfId) {
-      selfId = data.selfId;
-    }
-    for (let i = 0; i < data.player.length; i++) {
-      new Player(data.player[i]);
-    }
-    for (let i = 0; i < data.projectile.length; i++) {
-      new Projectile(data.projectile[i]);
-    }
-  });
-
-  //update
-
-  // loops through player and projectile lists, checks if defined, then updates values.
-  // if pack is undefined then there's no new information to update or a desync has occurred.
-  // expect: {player : [{id:123,number:'1',x:0,y:0},{id:1,number:'2',x:0,y:0}], projectile:[]}
-  socket.on('update', (data) => {
-    for (let i = 0; i < data.player.length; i++) {
-      let pack = data.player[i];
-      let p = Player.list[pack.id];
-      if (p) {
-        if (pack.x !== undefined) {
-          p.x = pack.x;
-        }
-        if (pack.y !== undefined) {
-          p.y = pack.y;
-        }
-        if (pack.hp !== undefined) {
-          p.hp = pack.hp;
-        }
-        if (pack.level !== undefined) {
-          p.level = pack.level;
-        }
-        if (pack.map !== undefined) {
-          p.map = pack.map;
+            ctx.drawImage(
+              Img.player,
+              0,
+              0,
+              Img.player.width,
+              Img.player.height,
+              x - width / 2,
+              y - height / 2,
+              width,
+              height
+            );
+          };
+          Player.list[self.id] = self;
+          return self;
         }
       }
-    }
-    for (let i = 0; i < data.projectile.length; i++) {
-      let pack = data.projectile[i];
-      let b = Projectile.list[data.projectile[i].id];
-      if (b) {
-        if (pack.x !== undefined) {
-          b.x = pack.x;
-        }
-        if (pack.y !== undefined) {
-          b.y = pack.y;
+
+      Player.list = {};
+
+      // initializes a Projectile using a package sent from server, contains all the data for client side
+
+      class Projectile {
+        constructor(initPack) {
+          let self = {};
+          self.id = initPack.id;
+          self.x = initPack.x;
+          self.y = initPack.y;
+          self.map = initPack.map;
+
+          self.draw = () => {
+            if (Player.list[selfId].map !== self.map) {
+              return;
+            }
+            let width = Img.projectile.width / 8;
+            let height = Img.projectile.height / 8;
+
+            let x = self.x - Player.list[selfId].x + WIDTH / 2;
+            let y = self.y - Player.list[selfId].y + HEIGHT / 2;
+
+            ctx.drawImage(
+              Img.projectile,
+              0,
+              0,
+              Img.projectile.width,
+              Img.projectile.height,
+              x - width / 2,
+              y - height / 2,
+              width,
+              height
+            );
+          };
+          Projectile.list[self.id] = self;
+          return self;
         }
       }
-    }
-  });
 
-  //remove
+      Projectile.list = {};
 
-  // Loops through the player list and projectile list and removes things designated for destruction.
-  // expect: {player : [12323], projectile:[12323,123123]}
-  socket.on('remove', (data) => {
-    for (let i = 0; i < data.player.length; i++) {
-      delete Player.list[data.player[i]];
-    }
-    for (let i = 0; i < data.projectile.length; i++) {
-      delete Projectile.list[data.projectile[i]];
-    }
-  });
+      // creates a new player and projectile and adds them to the player and projectile list
+      // expect: {player : [{id:123,number:'1',x:0,y:0},{id:1,number:'2',x:0,y:0}], projectile:[]}
+      let selfId = null;
 
-  // Positional awareness
+      socket.on('init', (data) => {
+        if (data.selfId) {
+          selfId = data.selfId;
+        }
+        for (let i = 0; i < data.player.length; i++) {
+          new Player(data.player[i]);
+        }
+        for (let i = 0; i < data.projectile.length; i++) {
+          new Projectile(data.projectile[i]);
+        }
+      });
 
-  // Loop through the players and projectile lists and draw them on the canvas 25 times per second.
-  setInterval((ctx, ctxUi) => {
-    if (!selfId) {
-      return;
-    }
-    drawMap(ctx);
-    drawLevel(ctxUi);
-    for (let i in Player.list) {
-      Player.list[i].draw();
-    }
-    for (let i in Projectile.list) {
-      Projectile.list[i].draw();
-    }
-  }, 40);
+      //update
 
-  let drawMap = (ctx) => {
-    let player = Player.list[selfId];
-    let x = WIDTH / 2 - player.x;
-    let y = HEIGHT / 2 - player.y;
-    ctx.drawImage(Img.map[player.map], x, y);
-  };
+      // loops through player and projectile lists, checks if defined, then updates values.
+      // if pack is undefined then there's no new information to update or a desync has occurred.
+      // expect: {player : [{id:123,number:'1',x:0,y:0},{id:1,number:'2',x:0,y:0}], projectile:[]}
+      socket.on('update', (data) => {
+        for (let i = 0; i < data.player.length; i++) {
+          let pack = data.player[i];
+          let p = Player.list[pack.id];
+          if (p) {
+            if (pack.x !== undefined) {
+              p.x = pack.x;
+            }
+            if (pack.y !== undefined) {
+              p.y = pack.y;
+            }
+            if (pack.hp !== undefined) {
+              p.hp = pack.hp;
+            }
+            if (pack.level !== undefined) {
+              p.level = pack.level;
+            }
+            if (pack.map !== undefined) {
+              p.map = pack.map;
+            }
+          }
+        }
+        for (let i = 0; i < data.projectile.length; i++) {
+          let pack = data.projectile[i];
+          let b = Projectile.list[data.projectile[i].id];
+          if (b) {
+            if (pack.x !== undefined) {
+              b.x = pack.x;
+            }
+            if (pack.y !== undefined) {
+              b.y = pack.y;
+            }
+          }
+        }
+      });
 
-  let drawLevel = (ctxUi) => {
-    if (lastLevel === Player.list[selfId].level) {
-      return;
+      //remove
+
+      // expect: {player : [12323], projectile:[12323,123123]}
+      socket.on('remove', (data) => {
+        for (let i = 0; i < data.player.length; i++) {
+          delete Player.list[data.player[i]];
+        }
+        for (let i = 0; i < data.projectile.length; i++) {
+          delete Projectile.list[data.projectile[i]];
+        }
+      });
+
+      // Positional awareness
+
+      // Loop through the players and projectile lists and draw them on the canvas.
+      setInterval(() => {
+        if (!selfId) {
+          return;
+        }
+        ctx.clearRect(0, 0, 500, 500);
+        drawMap();
+        // drawLevel();
+        for (let i in Player.list) {
+          Player.list[i].draw();
+        }
+        for (let i in Projectile.list) {
+          Projectile.list[i].draw();
+        }
+      }, 40);
+
+      let drawMap = () => {
+        let player = Player.list[selfId];
+        let x = WIDTH / 2 - player.x;
+        let y = HEIGHT / 2 - player.y;
+        ctx.drawImage(Img.map[player.map], x, y);
+      };
+
+      // let drawLevel = () => {
+      //   if (lastLevel === Player.list[selfId].level) {
+      //     return;
+      //   }
+      //   lastLevel = Player.list[selfId].level;
+      //   ctxUi.fillStyle = 'black';
+      //   ctxUi.fillText(Player.list[selfId].level, 0, 30);
+      // };
+      // let lastLevel = null;
     }
-    lastLevel = Player.list[selfId].level;
-    ctxUi.clearRect(0, 0, 500, 500);
-    ctxUi.fillStyle = 'black';
-    ctxUi.fillText(Player.list[selfId].level, 0, 30);
-  };
-  let lastLevel = null;
+  }, [canvasRef]);
 
   document.onkeydown = (event) => {
     if (event.key === 'd') {
+      // console.log('pushing d');
       socket.emit('keyPress', { inputId: 'right', state: true });
     } else if (event.key === 's') {
+      // console.log('pushing s');
       socket.emit('keyPress', { inputId: 'down', state: true });
     } else if (event.key === 'a') {
+      // console.log('pushing a');
       socket.emit('keyPress', { inputId: 'left', state: true });
     } else if (event.key === 'w') {
+      // console.log('pushing w');
       socket.emit('keyPress', { inputId: 'up', state: true });
     }
   };
@@ -238,6 +305,7 @@ const Game = () => {
   };
 
   document.onmousedown = (event) => {
+    // console.log('pushing attack');
     socket.emit('keyPress', { inputId: 'attack', state: true });
   };
   document.onmouseup = (event) => {
@@ -263,7 +331,7 @@ const Game = () => {
         // style="position: absolute; top: 8px; left: 8px; width:500px; height:500px"
       >
         <canvas ref={canvasRef}></canvas>
-        <canvas ref={uiRef}></canvas>
+        {/* <canvas ref={uiRef}></canvas> */}
 
         <div id="ui">
           <button>Change Map</button>
