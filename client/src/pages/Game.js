846c0b3b6c958c0820c9214732f4e46bd9f9a4e6
Mattadin <m4tt4d1n@gmail.com>
2022-07-26 16:17:58 +1000

    Copy-paste from retro-palace for debugging

--- client/src/pages/Game.js
+++ client/src/pages/Game.js
@@ -13,7 +13,7 @@ import { useQuery } from '@apollo/client';
 
 import { Inventory, Item } from '../Inventory';
 
-import { io } from 'https://cdn.socket.io/4.4.1/socket.io.esm.min.js';
+import { io } from 'socket.io-client';
 
 const Game = () => {
   const WIDTH = 500;
@@ -61,214 +61,177 @@ const Game = () => {
       //   chatInput.value = '';
       // };
 
-      // UI Functions
-
-      // const changeMap = () => {
-      //   socket.emit('changeMap');
-      // };
-
-      let inventory = new Inventory(socket, false);
-      socket.on('updateInventory', (items) => {
-        inventory.items = items;
-        inventory.refreshRender();
-      });
-
-      // game stuff
-
-      // init
-
-      // initializes a player using a package sent from server, contains all the data for client side
-
       class Player {
         constructor(initPack) {
-          let self = {};
-          self.id = initPack.id;
-          self.number = initPack.number;
-          self.x = initPack.x;
-          self.y = initPack.y;
-          self.hp = initPack.hp;
-          self.hpMax = initPack.hpMax;
-          self.level = initPack.level;
-          self.map = initPack.map;
-          // draws our characters, their hp bars and their levels.
-          self.draw = () => {
-            if (Player.list[selfId].map !== self.map) {
-              return;
+        let self = {};
+        self.id = initPack.id;
+        self.number = initPack.number;
+        self.x = initPack.x;
+        self.y = initPack.y;
+        self.hp = initPack.hp;
+        self.hpMax = initPack.hpMax;
+        self.level = initPack.level;
+        self.map = initPack.map;
+         // draws our characters, their hp bars and their levels.
+        self.draw = ()=> {
+            if(Player.list[selfId].map !== self.map) {
+                return;
             }
-            let x = self.x - Player.list[selfId].x + WIDTH/2;
-            let y = self.y - Player.list[selfId].y + HEIGHT/2;
+            let x= self.x - Player.list[selfId].x + WIDTH/2;
+            let y = self.y - Player.list[selfId].y + HEIGHT /2;
 
-            let width = Img.player.width /16;
-            let height = Img.player.height /16;
+            let width = Img.player.width/16;
+            let height = Img.player.height/16;
 
-            let hpWidth = (30 * self.hp) / self.hpMax;
+            let hpWidth = 30 * self.hp / self.hpMax;
             ctx.fillStyle = 'red';
             ctx.fillRect(x - hpWidth/2, y - 40, hpWidth, 4);
-
             // setting parameters for how to use the player images to render our user's sprites
 
-            ctx.drawImage(
-              Img.player,
-              0,
-              0,
-              Img.player.width,
-              Img.player.height,
-              x-width/2,
-              y-height/2,
-              width,
-              height
-            );
-          };
-          Player.list[self.id] = self;
-          return self;
+            ctx.drawImage(Img.player,
+            0, 0, Img.player.width, Img.player.height,
+            x-width/2, y-height/2, width, height);
         }
-      }
+        Player.list[self.id] = self;
+        return self;
+    }
+    }
 
-      Player.list = {};
+    Player.list = {};
 
-      // initializes a Projectile using a package sent from server, contains all the data for client side
+    // initializes a Projectile using a package sent from server, contains all the data for client side
 
-      class Projectile {
+    class Projectile {
         constructor(initPack) {
-          let self = {};
-          self.id = initPack.id;
-          self.x = initPack.x;
-          self.y = initPack.y;
-          self.map = initPack.map;
+        let self = {};
+        self.id = initPack.id;
+        self.x = initPack.x;
+        self.y = initPack.y;
+        self.map = initPack.map;
 
-          self.draw = () => {
-            if (Player.list[selfId].map !== self.map) {
-              return;
+        self.draw = ()=> {
+            if(Player.list[selfId].map !== self.map) {
+                return;
             }
             let width = Img.projectile.width/8;
             let height = Img.projectile.height/8;
 
-            let x = self.x - Player.list[selfId].x + WIDTH/2;
-            let y = self.y - Player.list[selfId].y + HEIGHT/2;
+            let x= self.x - Player.list[selfId].x + WIDTH/2;
+            let y = self.y - Player.list[selfId].y + HEIGHT /2;
 
-            ctx.drawImage(
-              Img.projectile,
-              0,
-              0,
-              Img.projectile.width,
-              Img.projectile.height,
-              x-width/2,
-              y-height/2,
-              width,
-              height
-            );
-          };
-          Projectile.list[self.id] = self;
-          return self;
+            ctx.drawImage(Img.projectile,
+            0, 0, Img.projectile.width, Img.projectile.height, x-width/2, y-height/2, width, height);
         }
-      }
+        Projectile.list[self.id] = self;
+        return self;
+    }
+    }
 
-      Projectile.list = {};
+    Projectile.list = {};
 
-      // creates a new player and projectile and adds them to the player and projectile list
-      // expect: {player : [{id:123,number:'1',x:0,y:0},{id:1,number:'2',x:0,y:0}], projectile:[]}
-      let selfId = null;
+    // creates a new player and projectile and adds them to the player and projectile list
+    // expect: {player : [{id:123,number:'1',x:0,y:0},{id:1,number:'2',x:0,y:0}], projectile:[]}
+    let selfId = null;
 
-      socket.on('init', (data) => {
-        if (data.selfId) {
-          selfId = data.selfId;
+    socket.on('init', (data)=> {
+        if(data.selfId) {
+            selfId = data.selfId;
         }
-        for (let i = 0; i < data.player.length; i++) {
-          new Player(data.player[i]);
+        for(let i = 0 ; i < data.player.length; i++) {
+            new Player (data.player[i]);
+        }
+        for(let i= 0; i < data.projectile.length; i++) {
+            new Projectile(data.projectile[i]);
+        }
+    })
+
+    //update
+
+    // loops through player and projectile lists, checks if defined, then updates values.
+    // if pack is undefined then there's no new information to update or a desync has occurred.
+    // expect: {player : [{id:123,number:'1',x:0,y:0},{id:1,number:'2',x:0,y:0}], projectile:[]}
+    socket.on('update', (data)=> {
+        for(let i = 0; i <data.player.length; i++) {
+            let pack = data.player[i];
+            let p = Player.list[pack.id];
+            if(p) {
+                if(pack.x !== undefined) {
+                    p.x = pack.x;
+                }
+                if(pack.y !== undefined) {
+                    p.y = pack.y;
+                }
+                if(pack.hp !== undefined) {
+                    p.hp = pack.hp;
+                }
+                if(pack.level !== undefined) {
+                    p.level = pack.level;
+                }
+                if(pack.map !== undefined) {
+                    p.map = pack.map;
+                }
+            }
         }
         for (let i = 0; i < data.projectile.length; i++) {
-          new Projectile(data.projectile[i]);
+            let pack = data.projectile[i];
+            let b = Projectile.list[data.projectile[i].id];
+            if(b) {
+                if(pack.x !== undefined) {
+                    b.x = pack.x;
+                }
+                if (pack.y !== undefined) {
+                    b.y = pack.y;
+                }
+            }
         }
-      });
+    })
 
-      //update
+    //remove
 
-      // loops through player and projectile lists, checks if defined, then updates values.
-      // if pack is undefined then there's no new information to update or a desync has occurred.
-      // expect: {player : [{id:123,number:'1',x:0,y:0},{id:1,number:'2',x:0,y:0}], projectile:[]}
-      socket.on('update', (data) => {
-        for (let i = 0; i < data.player.length; i++) {
-          let pack = data.player[i];
-          let p = Player.list[pack.id];
-          if (p) {
-            if (pack.x !== undefined) {
-              p.x = pack.x;
-            }
-            if (pack.y !== undefined) {
-              p.y = pack.y;
-            }
-            if (pack.hp !== undefined) {
-              p.hp = pack.hp;
-            }
-            if (pack.level !== undefined) {
-              p.level = pack.level;
-            }
-            if (pack.map !== undefined) {
-              p.map = pack.map;
-            }
-          }
+    // expect: {player : [12323], projectile:[12323,123123]}
+    socket.on('remove', (data)=> {
+        for(let i = 0; i < data.player.length; i++) {
+            delete Player.list[data.player[i]];
         }
-        for (let i = 0; i < data.projectile.length; i++) {
-          let pack = data.projectile[i];
-          let b = Projectile.list[data.projectile[i].id];
-          if (b) {
-            if (pack.x !== undefined) {
-              b.x = pack.x;
-            }
-            if (pack.y !== undefined) {
-              b.y = pack.y;
-            }
-          }
+        for(let i = 0; i < data.projectile.length; i++) {
+            delete Projectile.list[data.projectile[i]];
         }
-      });
+    });
 
-      //remove
+    // Positional awareness
 
-      // expect: {player : [12323], projectile:[12323,123123]}
-      socket.on('remove', (data) => {
-        for (let i = 0; i < data.player.length; i++) {
-          delete Player.list[data.player[i]];
-        }
-        for (let i = 0; i < data.projectile.length; i++) {
-          delete Projectile.list[data.projectile[i]];
-        }
-      });
-
-      // Positional awareness
-
-      // Loop through the players and projectile lists and draw them on the canvas.
-      setInterval(() => {
-        if (!selfId) {
-          return;
-        }
-        ctx.clearRect(0, 0, 500, 500);
+    // Loop through the players and projectile lists and draw them on the canvas.
+    setInterval(()=> {
+        if(!selfId) {
+            return
+        };
+        ctx.clearRect(0,0,500,500);
         drawMap();
         drawLevel();
-        for (let i in Player.list) {
-          Player.list[i].draw();
+        for(let i in Player.list) {
+            Player.list[i].draw();
         }
-        for (let i in Projectile.list) {
-          Projectile.list[i].draw();
+        for(let i in Projectile.list) {
+            Projectile.list[i].draw();
         }
-      }, 40);
+    }, 40);
 
-      let drawMap = () => {
+    let drawMap = ()=> {
         let player = Player.list[selfId];
         let x = WIDTH/2 - player.x;
         let y = HEIGHT/2 - player.y;
         ctx.drawImage(Img.map[player.map], x, y);
-      };
+    }
 
-      let drawLevel = () => {
-        if (lastLevel === Player.list[selfId].level) {
-          return;
+    let drawLevel = ()=> {
+        if(lastLevel === Player.list[selfId].level) {
+            return;
         }
         lastLevel = Player.list[selfId].level;
-        ctxUi.clearRect(0, 0, 500, 500);
         ctxUi.fillStyle = 'black';
-        ctxUi.fillText(Player.list[selfId].level, 0, 30);
-      };
-      let lastLevel = null;
+        ctxUi.fillText(Player.list[selfId].level,0,30);
+    }
+    let lastLevel = null;
     }
   }, [canvasRef, uiRef]);
 
--- server/server.js
+++ server/server.js
@@ -6,8 +6,8 @@ const server = require('http').Server(app);
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 const io = require('socket.io')(server, {});
-const { Entity, Player, Projectile } = require('./Entities');
-const { Inventory, Item } = require('../client/src/Inventory');
+// const { Entity, Player, Projectile } = require('./Entities');
+// const { Inventory, Item } = require('../client/src/Inventory');
 const { authMiddleware } = require('./utils/auth');
 const apolServer = new ApolloServer({
   typeDefs,
@@ -48,6 +48,271 @@ startApolloServer(typeDefs, resolvers);
 
 let SOCKET_LIST = {};
 
+Entity = (param) => {
+  let self = {
+    x: 250,
+    y: 250,
+    spdX: 0,
+    spdY: 0,
+    id: '',
+    map: 'map',
+  };
+  if (param) {
+    if (param.x) {
+      self.x = param.x;
+    }
+    if (param.y) {
+      self.y = param.y;
+    }
+    if (param.map) {
+      self.map = param.map;
+    }
+    if (param.id) {
+      self.id = param.id;
+    }
+  }
+  self.update = () => {
+    self.updatePosition();
+  };
+  self.updatePosition = () => {
+    self.x += self.spdX;
+    self.y += self.spdY;
+  };
+  // finds a point (pt) and finds the distance with a square root
+  self.getDistance = (pt) => {
+    return Math.sqrt(Math.pow(self.x - pt.x, 2) + Math.pow(self.y - pt.y, 2));
+  };
+  return self;
+};
+
+Player = (param) => {
+  let self = Entity(param);
+  self.number = '' + Math.floor(10 * Math.random());
+  self.pressingRight = false;
+  self.pressingLeft = false;
+  self.pressingUp = false;
+  self.pressingDown = false;
+  self.pressingAttack = false;
+  self.mouseAngle = 0;
+  self.maxSpd = 10;
+  self.hp = 100;
+  self.hpMax = 100;
+  self.level = 0;
+
+  let super_update = self.update;
+  self.update = () => {
+    self.updateSpd();
+    super_update();
+
+    if (self.pressingAttack) {
+      self.shootProjectile(self.mouseAngle);
+    }
+  };
+  self.shootProjectile = (angle) => {
+    Projectile({
+      shooter: self.id,
+      angle: angle,
+      x: self.x,
+      y: self.y,
+      map: self.map,
+    });
+  };
+
+  self.updateSpd = () => {
+    if (self.pressingRight) {
+      self.spdX = self.maxSpd;
+    } else if (self.pressingLeft) {
+      self.spdX = -self.maxSpd;
+    } else {
+      self.spdX = 0;
+    }
+    if (self.pressingUp) {
+      self.spdY = -self.maxSpd;
+    } else if (self.pressingDown) {
+      self.spdY = self.maxSpd;
+    } else {
+      self.spdY = 0;
+    }
+  };
+
+  self.getInitPack = () => {
+    return {
+      id: self.id,
+      x: self.x,
+      y: self.y,
+      number: self.number,
+      hp: self.hp,
+      hpMax: self.hpMax,
+      level: self.level,
+      map: self.map,
+    };
+  };
+
+  self.getUpdatePack = () => {
+    return {
+      id: self.id,
+      x: self.x,
+      y: self.y,
+      hp: self.hp,
+      level: self.level,
+      map: self.map,
+    };
+  };
+
+  Player.list[self.id] = self;
+
+  initPack.player.push(self.getInitPack());
+  return self;
+};
+
+Player.list = {};
+
+Player.onConnect = (socket) => {
+  let map = 'tundra';
+  if (Math.random() < 0.5) {
+    map = 'palace';
+  }
+  let player = Player({
+    id: socket.id,
+    map: map,
+  });
+  socket.on('keyPress', (data) => {
+    if (data.inputId === 'left') {
+      player.pressingLeft = data.state;
+    } else if (data.inputId === 'right') {
+      player.pressingRight = data.state;
+    } else if (data.inputId === 'up') {
+      player.pressingUp = data.state;
+    } else if (data.inputId === 'down') {
+      player.pressingDown = data.state;
+    } else if (data.inputId === 'attack') {
+      player.pressingAttack = data.state;
+    } else if (data.inputId === 'mouseAngle') {
+      player.mouseAngle = data.state;
+    }
+  });
+
+  socket.on('changeMap', (data) => {
+    if (player.map === 'tundra') {
+      player.map = 'palace';
+    } else {
+      player.map = 'tundra';
+    }
+  });
+
+  socket.emit('init', {
+    selfId: socket.id,
+    player: Player.getAllInitPack(),
+    projectile: Projectile.getAllInitPack(),
+  });
+};
+
+Player.getAllInitPack = () => {
+  let players = [];
+  for (let i in Player.list) {
+    players.push(Player.list[i].getInitPack());
+  }
+  return players;
+};
+
+Player.onDisconnect = (socket) => {
+  delete Player.list[socket.id];
+  removePack.player.push(socket.id);
+};
+
+Player.update = () => {
+  let pack = [];
+  for (let i in Player.list) {
+    let player = Player.list[i];
+    player.update();
+    pack.push(player.getUpdatePack());
+  }
+  return pack;
+};
+
+Projectile = (param) => {
+  let self = Entity(param);
+  self.id = Math.random();
+  self.angle = param.angle;
+  self.spdX = Math.cos((param.angle / 180) * Math.PI) * 20;
+  self.spdY = Math.sin((param.angle / 180) * Math.PI) * 20;
+  self.shooter = param.shooter;
+
+  self.timer = 0;
+  self.toRemove = false;
+  let super_update = self.update;
+  self.update = function () {
+    if (self.timer++ > 20) {
+      self.toRemove = true;
+    }
+    super_update();
+
+    for (let i in Player.list) {
+      let p = Player.list[i];
+      if (
+        self.map === p.map &&
+        self.getDistance(p) < 32 &&
+        self.shooter !== p.id
+      ) {
+        p.hp -= 5;
+        if (p.hp <= 0) {
+          let shooter = Player.list[self.shooter];
+          if (shooter) {
+            shooter.level += 1;
+          }
+          p.hp = p.hpMax;
+          p.x = Math.random() * 500;
+          p.y = Math.random() * 500;
+        }
+        self.toRemove = true;
+      }
+    }
+  };
+  self.getInitPack = () => {
+    return {
+      id: self.id,
+      x: self.x,
+      y: self.y,
+      map: self.map,
+    };
+  };
+
+  self.getUpdatePack = () => {
+    return {
+      id: self.id,
+      x: self.x,
+      y: self.y,
+    };
+  };
+  Projectile.list[self.id] = self;
+  initPack.projectile.push(self.getInitPack());
+  return self;
+};
+Projectile.list = {};
+
+Projectile.update = () => {
+  let pack = [];
+  for (let i in Projectile.list) {
+    let projectile = Projectile.list[i];
+    projectile.update();
+    if (projectile.toRemove === true) {
+      delete Projectile.list[i];
+      removePack.projectile.push(projectile.id);
+    } else {
+      pack.push(projectile.getUpdatePack());
+    }
+  }
+  return pack;
+};
+
+Projectile.getAllInitPack = () => {
+  let projectiles = [];
+  for (let i in Projectile.list) {
+    projectiles.push(Projectile.list[i].getInitPack());
+  }
+  return projectiles;
+};
+
 // set me to false on release!
 const DEBUG = true;
 
@@ -60,31 +325,45 @@ io.sockets.on('connection', (socket) => {
     Player.onDisconnect(socket);
   });
 
-  // socket.on('signIn', (data) => {
-  //   isValidPassword(data, (res) => {
-  //     if (res) {
-  //       Player.onConnect(socket, data.username);
-  //       socket.emit('signInResponse', { success: true });
-  //     } else {
-  //       socket.emit('signInResponse', { success: false });
-  //     }
-  //   });
-  // });
-
   Player.onConnect(socket);
   socket.on('disconnect', () => {
     delete SOCKET_LIST[socket.id];
     Player.onDisconnect(socket);
   });
+  socket.on('sendMsgToServer', (data) => {
+    let playerName = ('' + socket.id).slice(2, 7);
+    for (let i in SOCKET_LIST) {
+      SOCKET_LIST[i].emit('addToChat', playerName + ': ' + data);
+    }
+    socket.on('evalServer', (data) => {
+      if (!DEBUG) {
+        return;
+      }
+      const result = eval(data);
+      socket.emit('evalAnswer', result);
+    });
+  });
+  console.log('socket connection');
 });
+
+let initPack = { player: [], projectile: [] };
+let removePack = { player: [], projectile: [] };
 
 // every frame update the game state and empty arrays to avoid duplication
 setInterval(() => {
-  const packs = Entity.getFrameUpdateData();
+  let pack = {
+    player: Player.update(),
+    projectile: Projectile.update(),
+  };
+
   for (let i in SOCKET_LIST) {
     let socket = SOCKET_LIST[i];
-    socket.emit('init', packs.initPack);
-    socket.emit('update', packs.updatePack);
-    socket.emit('remove', packs.removePack);
+    socket.emit('init', initPack);
+    socket.emit('update', pack);
+    socket.emit('remove', removePack);
   }
+  initPack.player = [];
+  initPack.projectile = [];
+  removePack.player = [];
+  removePack.projectile = [];
 }, 40);
