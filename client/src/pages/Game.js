4a275f115d4e613fbe0d63d7e24e5b45bbe7bda8
Mattadin <m4tt4d1n@gmail.com>
2022-08-04 18:27:59 +1000

    Last minute heroku fix

--- client/src/pages/Game.js
+++ client/src/pages/Game.js
@@ -11,6 +11,7 @@ import Auth from '../utils/auth'
 // import Player from '../classes/Player';
 // import Projectile from '../classes/Projectile';
 import { ChoiceContext } from '../utils/Context';
+import './game.css'
 
 // Import the `useParams()` hook
 import { useParams } from 'react-router-dom';
